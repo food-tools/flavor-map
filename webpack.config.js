7945685feead71baea58d6bdf0dcd9fcd6c9f470
Brett Fouss <bmfouss@gmail.com>
2018-10-08 12:59:16 -0400

    More config changes (hot reload not working for some reason)

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRootPlugin = require('html-webpack-react-root-plugin');
 module.exports = {
     entry: "./src/index.jsx",
     mode: "development",
     devtool: "source-map",
+    devServer: {
+        contentBase: "dist"
+    },
     output: {
         filename: "bundle.js",
-        publicPath: "/",
-        path: __dirname + "/dist"
-    },
-    devServer: {
-        contentBase: './'
+        path: path.resolve(__dirname, 'dist/')
     },
     resolve: {
         extensions: [".js", ".jsx", ".json"]
     },
     module: {
         rules: [
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+            { enforce: "pre", test: /\.(js|jsx$)/, loader: "source-map-loader" },
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: ["@babel/env", "@babel/react"] }}
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin(), new ReactRootPlugin()
+    ]
 };
