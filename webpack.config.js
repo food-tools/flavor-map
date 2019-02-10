c6a5948a31bb659b6bfee06ea25e1fc9a6b86899
brettfouss <bmfouss@gmail.com>
2019-02-10 10:42:15 -0500

    Update spacing in webpack config for improved readability

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const apiMocker = require("mocker-api");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: path.resolve(__dirname, "src", "index.jsx"),
@@ -11,14 +11,45 @@ module.exports = {
     },
     devtool: "source-map",
     resolve: {
-        extensions: [".js", ".jsx", ".json", ".html", ".css"]
+        extensions: [
+            ".js",
+            ".jsx",
+            ".json",
+            ".html",
+            ".css"
+        ]
     },
     module: {
         rules: [
-            { enforce: "pre", test: /\.(js|jsx$)/, loader: "source-map-loader" },
-            { test: /\.css$/, loader: "style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5" },
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader", options: { presets: ["@babel/env", "@babel/react"], plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/plugin-transform-regenerator", "@babel/plugin-transform-runtime"] }},
-            { test: /\.(png|svg|jpg|gif|ico)$/, loader: "file-loader" }
+            {
+                enforce: "pre",
+                test: /\.(js|jsx$)/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5"
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        "@babel/env",
+                        "@babel/react"
+                    ],
+                    plugins: [
+                        "@babel/plugin-proposal-object-rest-spread",
+                        "@babel/plugin-transform-regenerator",
+                        "@babel/plugin-transform-runtime"
+                    ]
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                loader: "file-loader"
+            }
         ]
     },
     devServer: {
@@ -30,7 +61,7 @@ module.exports = {
         },
         before(app) {
             apiMocker(app, path.resolve('./mocker/index.js'), {
-                changeHost: true,
+                changeHost: true
             })
         }
     }
