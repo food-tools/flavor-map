1f132adc58853b6339b2a005234beb358c8b9588
brettfouss <bmfouss@gmail.com>
2019-02-11 18:15:08 -0500

    Pass callbacks for node hover and click, as well as selected and hovered node state to d3 code

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,15 +1,22 @@
 import { connect } from "react-redux";
 import { MapContainer } from "../components/MapContainer";
+import { setHoveredNode, setSelectedNode } from "../actions/actions";
 
 const mapStateToProps = (state, ownProps) => {
     return {
         ingredients: state.results.ingredients.items.map(id => state.data.ingredients[id]),
-        pairings: state.data.pairings
+        pairings: state.data.pairings,
+        selectedNode: state.options.selectedNode,
+        hoveredNode: state.options.hoveredNode
     }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {}
+    return {
+        onNodeMouseOver: (id) => dispatch(setHoveredNode(id)),
+        onNodeMouseOut: (id) => dispatch(setHoveredNode(null)),
+        onNodeClick: (id) => dispatch(setSelectedNode(id))
+    }
 }
 
 export const Map = connect(
