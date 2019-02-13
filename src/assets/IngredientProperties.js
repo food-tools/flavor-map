7b5b842a8dec5d675d7e4f7d6ccdba0bde1b121f
brettfouss <bmfouss@gmail.com>
2019-02-13 17:19:37 -0500

    Use exported objects for ingredient properties and color enumerations

---
+++ src/assets/IngredientProperties.js
@@ -0,0 +1,38 @@
+export const IngredientTypes = {
+    VEGETABLE: "VEGETABLE",
+    FRUIT: "FRUIT",
+    GRAIN: "GRAIN",
+    DAIRY: "DAIRY",
+    FAT: "FAT",
+    NUT: "NUT",
+    MEAT: "MEAT",
+    HERB: "HERB",
+    SPICE: "SPICE"
+};
+
+export const IngredientSeasons = {
+    SPRING: "SPRING",
+    SUMMER: "SUMMER",
+    AUTUMN: "AUTUMN",
+    WINTER: "WINTER"
+};
+
+export const IngredientTastes = {
+    SALTY: "SALTY",
+    SUMMER: "SUMMER",
+    AUTUMN: "AUTUMN",
+    WINTER: "WINTER"
+};
+
+export const IngredientWeights = {
+    HEAVY: "HEAVY",
+    MEDIUM: "MEDIUM",
+    LIGHT: "LIGHT"
+};
+
+export const IngredientVolumes = {
+    QUIET: "QUIET",
+    MODERATE: "MODERATE",
+    MODERATE_LOUD: "MODERATE_LOUD",
+    LOUD: "LOUD"
+};
---
+++ src/assets/IngredientPropertyColors.js
@@ -0,0 +1,15 @@
+import {
+    IngredientTypes
+} from "./IngredientProperties";
+
+export const IngredientTypeColors = {
+    [IngredientTypes.VEGETABLE]: "#14453d",
+    [IngredientTypes.FRUIT]: "#a3333d",
+    [IngredientTypes.GRAIN]: "#c4a69d",
+    [IngredientTypes.DAIRY]: "#7391ba",
+    [IngredientTypes.FAT]: "#aa9052",
+    [IngredientTypes.NUT]: "#363457",
+    [IngredientTypes.MEAT]: "#6d3b47",
+    [IngredientTypes.HERB]: "#98a886",
+    [IngredientTypes.SPICE]: "#ba5734"
+};
